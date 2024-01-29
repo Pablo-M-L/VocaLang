c16e1229b7de2ba8b9f1899c756b442bcccd212a
Pablo-M-L <crapula8@hotmail.com>
2024-01-29 20:04:23 +0100

    Initial Commit

---
+++ VocaLang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VocaLang
+//
+//  Created by pablo millan lopez on 29/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VocaLang/VocaLangApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocaLangApp.swift
+//  VocaLang
+//
+//  Created by pablo millan lopez on 29/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VocaLangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
